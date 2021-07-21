92fd24cd403eecec0d9fea26894cc5e635b781c6
lakhara <siddharth.lakhara@gmail.com>
2021-07-21 18:34:02 +0530

    add basic client side routing

--- frontend/static/js/index.js
+++ frontend/static/js/index.js
@@ -1,3 +1,35 @@
 // Root of client side JS
 
-console.log('JS is loaded');
+// Client side router
+const router = async () => {
+  const routes = [
+    { path: '/', view: () => { console.log('Viewing dashboard'); } },
+    { path: '/posts', view: () => { console.log('Viewing posts'); } },
+    { path: '/settings', view: () => { console.log('Viewing settings'); } },
+  ];
+
+  // Test each route for potential match
+  // TODO: Remove this in final version
+  const potentialMatches = routes.map((route) => ({
+    route,
+    isMatch: window.location.pathname === route.path,
+  }));
+
+  let match = potentialMatches.find((potentialMatch) => (potentialMatch.isMatch));
+
+  // handle 404 routes
+  if (!match) {
+    // redirect to home page
+    // TODO: create a 404 page
+    match = {
+      route: routes[0],
+    };
+  }
+
+  match.route.view();
+  // console.log();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  router();
+});
